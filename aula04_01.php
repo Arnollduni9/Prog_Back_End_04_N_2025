b3912dd2ca81a1b928acf259fd0cc05192e393e0
Arnollduni9 <arnolld500@uni9.edu.br>
2025-09-11 00:38:29 +0000

    Aula5

--- aula04_01.php
+++ aula04_01.php
@@ -3,28 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula04_01</title>
+    <title>aula05_01</title>
 </head>
 <body>
-    <h1>Aula04_01 - 03/09/25</h1>
-    <?php
-    $nome="Bete ";
-    $dia=3;
-    $nota=8.9;
-    echo "$nome, no dia $dia tirou a nota: $nota<br>";
-    //var_dump($nome);
-    define("USER","admin");
-    echo "User: ". USER;
-    //User: admin
-    $cep="01012123";//
-    echo "<br>Cep: $cep <br>";
-    echo '<br>Cep: $cep <br>';
-    /*
-    base decimal 0123456789 10
-    base octal   01234567 10
-    base binario 01 10
-    base hexadecimal 0123456789ABCDEF 10 
-    */
-    ?>
+    <h1>AULA 05_01</h1>
+    <form method="post" action="aula05_02.php">
+        Login <input type="text" name="login"><br>
+        senha <input type="password" name="senha"><br>
+        <input type="submit" value="Enviar">
+    </form>
+    
+    
 </body>
 </html>
